5ff8f4c2cc42fba4f07dd85aca2efd4f796a8062
Fravim <Flaviomorillaalves@gmail.com>
2026-01-13 23:12:07 -0300

    Teste email

--- src/XadrezJogo/Aplicacao/Main.java
+++ src/XadrezJogo/Aplicacao/Main.java
@@ -7,7 +7,7 @@ import XadrezJogo.Xadrez.XadrezPosicao;
 import java.util.Scanner;
 
 public class Main {
-
+//testeemail
 
     public static void main(String[] args) {
         XadrezPartida xadrezPartida = new XadrezPartida();
